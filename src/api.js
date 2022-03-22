081324b4859c7b223d49d2ebbb26144223a3bdce
Raphael Araujo <raphaelwilliam.araujo@gmail.com>
2022-03-22 10:49:10 -0300

    validate token

--- src/api.js
+++ src/api.js
@@ -11,7 +11,19 @@ export function TOKEN_POST(body) {
       body: JSON.stringify(body),
     },
   };
-};
+}
+
+export function TOKEN_VALIDATE_POST(token) {
+  return {
+    url: API_URL + '/jwt-auth/v1/token/validate',
+    options: {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+}
 
 export function USER_GET(token) {
   return {
