96b71b80901bb3420ddf72db5bf388a71c3d7447
Raphael Araujo <raphaelwilliam.araujo@gmail.com>
2022-03-22 11:46:10 -0300

    create login criar conta

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 const types = {
   email: {
-    regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-    Message: 'Preencha um email válido!',
-  }
-}
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido',
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+  },
+};
 
 const useForm = (type) => {
-  const [ value, setValue ] = React.useState('');
-  const [ error, setError ] = React.useState(null);
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
 
   function validate(value) {
     if (type === false) return true;
@@ -26,8 +31,8 @@ const useForm = (type) => {
   }
 
   function onChange({ target }) {
-    if (error) setValue( target.value );
-    setValue( target.value );
+    if (error) validate(target.value);
+    setValue(target.value);
   }
 
   return {
